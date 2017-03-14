a574b9442a4fbd2e9205564b869db18c69eda7b4
Emmanuel Díaz <emmanuel.diaz@cup.local>
2017-03-14 20:22:42 -0300

    ultima version con Edicionde Noticias

--- notibad/urls.py
+++ notibad/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
      url(r'^$',views.notibad_list),
      url(r'^noticia/(?P<pk>[0-9]+)/$', views.noticia_detail),
+     url(r'^noticia/new/$', views.noticia_new, name='noticia_new'),
+     url(r'^noticia/(?P<pk>[0-9]+)/edit/$', views.noticia_edit, name='noticia_edit'),
 ]
--- notibad/views.py
+++ notibad/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Noticia
+from .forms import NoticiaForm
 
 def notibad_list(request):
 	qs_noticia = Noticia.objects.all()
@@ -8,3 +9,30 @@ def notibad_list(request):
 def noticia_detail(request,pk):
 	noticia = Noticia.objects.get(pk=pk)
 	return render(request,r'notibad\noticia_detail.html',{'noticia':noticia})
+
+def noticia_new(request):
+	if request.method == 'POST':
+		form = NoticiaForm(request.POST)
+		if form.is_valid():
+			noticia = form.save(commit=False)#para no guardar aun, indicando que vamos a agregar datos
+			noticia.author = request.user
+			noticia.save()
+			return redirect(r'notibad.views.noticia_detail',pk=noticia.pk)
+	else:
+		form = NoticiaForm()
+	return render(request,r'notibad\noticia_edit.html',{'form':form})
+
+def noticia_edit(request,pk):
+	noticia = get_object_or_404(Noticia,pk=pk)
+	if request.method == 'POST':
+		form = NoticiaForm(request.POST,instance=noticia)
+		if form.is_valid():
+			noticia = form.save(commit=False)#para no guardar aun, indicando que vamos a agregar datos
+			noticia.author = request.user
+			noticia.save()
+			return redirect(r'notibad.views.noticia_detail',pk=noticia.pk)
+	else:
+		form = NoticiaForm(instance=noticia)
+	return render(request,r'notibad\noticia_edit.html',{'form':form})
+
+
